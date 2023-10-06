99c3f471e9233c3c5df2dfcf9fa2c507ef8806f8
0xshoegazer <0xshoegazer@gmail.com>
2023-10-06 11:08:59 -0400

    deploys

--- scripts/core/deployOrderBook.js
+++ scripts/core/deployOrderBook.js
@@ -1,32 +1,36 @@
-const { deployContract, contractAt , sendTxn, writeTmpAddresses } = require("../shared/helpers")
-const { expandDecimals } = require("../../test/shared/utilities")
+const { deployContract, contractAt, sendTxn, writeTmpAddresses } = require('../shared/helpers');
+const { expandDecimals } = require('../../test/shared/utilities');
 
-const network = (process.env.HARDHAT_NETWORK || 'mainnet');
+const network = process.env.HARDHAT_NETWORK || 'mainnet';
 const tokens = require('./tokens')[network];
 
 async function main() {
-  const { nativeToken } = tokens
+  const { nativeToken } = tokens;
 
-  const orderBook = await deployContract("OrderBook", []);
+  // const orderBook = await deployContract('OrderBook', []);
+  const orderBook = await contractAt('OrderBook', '0x96E73B10d0f4b7180c7C89456E984dFF6C73EB3f');
 
   // Arbitrum mainnet addresses
-  await sendTxn(orderBook.initialize(
-    "0x5F719c2F1095F7B9fc68a68e35B51194f4b6abe8", // router
-    "0x9ab2De34A33fB459b538c43f251eB825645e8595", // vault
-    nativeToken.address, // weth
-    "0xc0253c3cC6aa5Ab407b5795a04c28fB063273894", // usdg
-    "10000000000000000", // 0.01 AVAX
-    expandDecimals(10, 30) // min purchase token amount usd
-  ), "orderBook.initialize");
+  await sendTxn(
+    orderBook.initialize(
+      '0x94265f90728993A3f7089049045ceef566A36bB9', // router
+      '0xaB1E8868FEe285cF5F379aCF61ae4D65211fc6FE', // vault
+      nativeToken.address, // weth
+      '0xba3FeA4Fe91de455164Fb9979B834ce4E11f274b', // usdg
+      '10000000000000000', // _minExecutionFee 0.01 Native ETH/AVAX
+      expandDecimals(10, 30) // min purchase token amount usd
+    ),
+    'orderBook.initialize'
+  );
 
   writeTmpAddresses({
-    orderBook: orderBook.address
-  })
+    orderBook: orderBook.address,
+  });
 }
 
 main()
   .then(() => process.exit(0))
-  .catch(error => {
-    console.error(error)
-    process.exit(1)
-  })
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
